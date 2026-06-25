98f8b14ba311aaba9af74cdc2270251554ef83a7
Rooshitha <rooshi7667@gmail.com>
2026-06-25 15:23:26 +0530

    added logging middleware

---
+++ logging-middleware/logger.js
@@ -0,0 +1,15 @@
+const logger = {
+  info(message) {
+    console.log(`[INFO] ${message}`);
+  },
+
+  error(message) {
+    console.error(`[ERROR] ${message}`);
+  },
+
+  warn(message) {
+    console.warn(`[WARN] ${message}`);
+  }
+};
+
+export default logger;
